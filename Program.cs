7a839ee6adb787bd7f183f7a70d8de61178b7b90
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2024-12-05 20:46:59 +0500

    feat: serialize configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿namespace NuhhlerAdventBot;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NuhhlerAdventBot;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+
+        var botConfig = config.GetSection("BotConfig").Get<BotConfig>();
+
+        Console.WriteLine(botConfig.BotToken);
     }
 }
