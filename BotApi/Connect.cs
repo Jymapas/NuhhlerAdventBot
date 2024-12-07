bd81e2b3c572f96ddb9d6fff33449deea69aea0a
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2024-12-07 16:25:29 +0500

    feat(Connect): add list of commands

--- BotApi/Connect.cs
+++ BotApi/Connect.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace NuhhlerAdventBot.BotApi;
@@ -18,6 +19,7 @@ public class Connect
 
         var botClient = new TelegramBotClient(botConfig.BotToken);
         using var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
 
         var receiverOptions = new ReceiverOptions
         {
@@ -26,14 +28,43 @@ public class Connect
 
         var handlers = new BotHandlers(botClient, botConfig);
 
+        var privateChatCommands = new List<BotCommand>
+        {
+            new BotCommand { Command = "set", Description = "Установить" },
+            new BotCommand { Command = "edit", Description = "Редактировать" },
+            new BotCommand { Command = "delete", Description = "Удалить" },
+            new BotCommand { Command = "check", Description = "Проверить" }
+        };
+
+        await botClient.SetMyCommands(
+            commands: privateChatCommands,
+            scope: new BotCommandScopeAllPrivateChats(),
+            cancellationToken: cancellationToken
+        );
+
+        Console.WriteLine("Команды для приватных чатов установлены.");
+
+        var groupChatCommands = new List<BotCommand>
+        {
+            new BotCommand { Command = "advent", Description = "Получить своё адвент-посллание" }
+        };
+
+        await botClient.SetMyCommands(
+            commands: groupChatCommands,
+            scope: new BotCommandScopeAllGroupChats(),
+            cancellationToken: cancellationToken
+        );
+
+        Console.WriteLine("Команды для групповых чатов установлены.");
+        
         botClient.StartReceiving(
             handlers.HandleUpdateAsync,
             handlers.HandleErrorAsync,
             receiverOptions,
-            cts.Token
+            cancellationToken
         );
 
-        var me = await botClient.GetMeAsync();
+        var me = await botClient.GetMe(cancellationToken);
         Console.WriteLine($"Бот @{me.Username} запущен и ожидает сообщений.");
 
         Console.ReadLine();
