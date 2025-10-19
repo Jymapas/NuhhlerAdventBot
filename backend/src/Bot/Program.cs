195003059aa2002ca9450f3377c4c54455d5d7dc
Jymapas <jymapas@yandex.ru>
2025-10-19 21:58:58 +0500

    chore(Bot): update Program.cs

--- backend/src/Bot/Program.cs
+++ backend/src/Bot/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using Application.Abstractions;
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Shared.Env;
+using Shared.Logging;
+using Telegram.Bot;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+SerilogBootstrap.ConfigureSerilog(builder);
+
+builder.Services.AddInfrastructure();
+
+var botToken = builder.Configuration[EnvKeys.BotToken] ?? "";
+if (string.IsNullOrWhiteSpace(botToken))
+    Console.WriteLine("WARNING: BOT_TOKEN is empty; Bot will run without Telegram connectivity.");
+
+builder.Services.AddSingleton(new TelegramBotClient(botToken));
+
+var app = builder.Build();
+
+var info = app.Services.GetRequiredService<IBotInfo>();
+Console.WriteLine($"Advent Bot (Bot) started; version: {info.Version}; OS: {RuntimeInformation.OSDescription}");
+
+await app.RunAsync();
