454c3dad7f30f55e986a04593ab7e2a730a5391a
Jymapas <jymapas@yandex.ru>
2025-10-19 22:00:03 +0500

    chore(Worker): update Program.cs

--- backend/src/Worker/Program.cs
+++ backend/src/Worker/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Application.Abstractions;
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Shared.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+SerilogBootstrap.ConfigureSerilog(builder);
+
+builder.Services.AddInfrastructure();
+
+builder.Services.AddHostedService<TickerService>();
+
+var app = builder.Build();
+
+var info = app.Services.GetRequiredService<IBotInfo>();
+Console.WriteLine($"Advent Bot (Worker) started; version: {info.Version}");
+await app.RunAsync();
+
+public sealed class TickerService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.WriteLine($"[Worker] tick {DateTime.UtcNow:O}");
+            await Task.Delay(TimeSpan.FromSeconds(60), stoppingToken);
+        }
+    }
+}
