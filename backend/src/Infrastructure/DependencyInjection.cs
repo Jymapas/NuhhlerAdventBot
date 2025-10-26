83f61593a6440cc6f6c045dea5f083412c93c39c
Aleksandr Petrakov <jymapas@yandex.ru>
2025-10-26 13:20:18 +0500

    chore(DI): add DeliveryService

--- backend/src/Infrastructure/DependencyInjection.cs
+++ backend/src/Infrastructure/DependencyInjection.cs
@@ -42,6 +42,7 @@ public static class DependencyInjection
         services.AddScoped<IImportService, ImportService>();
         services.AddScoped<ITemplateGenerator, TemplateGenerator>();
         services.AddScoped<TimeSetupService>();
+        services.AddScoped<DeliveryService>();
 
         return services;
     }
